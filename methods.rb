6abe7676f61e4469841ec3f6989c55f1a13ace99
MrChriss <mrchriss@localhost.localdomain>
2016-06-27 17:13:54 +0200

    LS exercises from chapter methods, all

---
+++ methods.rb
@@ -0,0 +1,29 @@
+#exercise n. 1
+def greeting(name)
+  "Hello " + name
+end
+puts greeting("XYZ")
+
+#exercise n. 2
+=begin
+x = 2                       evaluates to 2
+puts x = 2                  evaluates to nil
+p name = "Joe"              evaluates to "Joe"
+four = "four"               evaluates to "four"
+print something = "nothing" evaluates to nil
+=end
+
+#exercise n. 3
+def multiply(x, y)
+  x * y
+end
+puts multiply(12, 25)
+
+#exercise n. 4
+#it will NOT print "Yippeee!!!!" to the screen
+
+#exercise n. 5
+#returns nil
+
+#exercise n. 6
+#method expected to get two arguments, but only got one
