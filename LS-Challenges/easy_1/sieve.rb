5d2680bac74cebd89c21cc855ba92c9406a2837a
MrChriss <kristof.crnivec@gmail.com>
2016-08-31 23:05:58 +0200

    adding better version of sieve challenge

--- LS-Challenges/easy_1/sieve.rb
+++ LS-Challenges/easy_1/sieve.rb
@@ -1,20 +1,4 @@
 class Sieve
-  attr_reader :limit
-
-  def initialize(limit)
-    @limit = limit
-  end
-
-  def primes
-    (2..limit).select do |num|
-      if (1..limit).to_a.map { |n| num % n == 0 }.count(true) == 2
-        true
-      end
-    end
-  end
-end
-
-class Sieve
   def initialize(limit)
     @limit = limit
   end
